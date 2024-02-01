9af2fef28954a401322ad3ebd1d4feb01453a9eb
Adilbek-byte <70812093+Adilbek-byte@users.noreply.github.com>
2024-02-01 21:04:53 +0600

    Flight #
    Добавьте файлы проекта.

---
+++ AirlineFlight/Controllers/AirFlightsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+
+namespace AirlineFlight.Controllers;
+
+[ApiController]
+[Route("flights")]
+public class AirFlightsController: ControllerBase
+{
+    public static List<Flight> planes = AirFlights.CreateFlights();
+    [HttpGet("flights")]
+    public List<Flight> GetFlights()
+    {
+        return planes;
+    }
+
+    
+}
---
+++ AirlineFlight/LocationPath.cs
@@ -0,0 +1,9 @@
+﻿namespace AirlineFlight;
+
+public class LocationPath
+{
+   
+    public string? FromWhere { get; set; }
+    public string? ToWhere { get; set; }
+
+}
